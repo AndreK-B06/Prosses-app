a38d5b42ae5540ea5997b2eaae4e5cb92f5b1a37
André <137167063+AndreK-B06@users.noreply.github.com>
2024-03-14 14:42:25 +0100

    preparation start for grid

--- Prosses-app/src/App.jsx
+++ Prosses-app/src/App.jsx
@@ -1,9 +1,15 @@
 import "./css/App.css";
+import "./css/buffer-tank.css";
 
 function App() {
   return (
     <>
-      <section id="water-container"></section>
+      <section id="grid-container">
+        <section className="Buffer-tank" id="buffer-top"></section>
+        <section className="Buffer-tank" id="buffer-mid-top"></section>
+        <section className="Buffer-tank" id="buffer-mid-bottom"></section>
+        <section className="Buffer-tank" id="buffer-bottom"></section>
+      </section>
     </>
   );
 }
