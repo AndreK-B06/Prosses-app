aaf766ebf1a29b22252666ee6d54b8fac139d1dc
André <137167063+AndreK-B06@users.noreply.github.com>
2024-06-10 14:40:57 +0200

    working on  progress-tank

--- src/components/ProgressTank/ProgressTank.jsx
+++ src/components/ProgressTank/ProgressTank.jsx
@@ -1,11 +1,7 @@
 export default function ProgressTank() {
   return (
     <>
-      <progress>
-        <section id="progress-tank">
-          <section id="progress-value"></section>
-        </section>
-      </progress>
+      <progress id="progress-tank"></progress>
     </>
   );
 }
